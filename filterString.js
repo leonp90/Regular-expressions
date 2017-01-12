6ac92f1ee2e018715c9f6476ba4ae335558395cf
Oscar Leon <oscar.l.p.7@gmail.com>
2017-01-13 00:06:07 +0100

    Regular expression filter

---
+++ filterString.js
@@ -0,0 +1,12 @@
+function string(S){
+	var str = S.replace(/[0-9]/g, " ");
+	var arr = str.split(" ");
+	var long = -1;
+	console.log(str);
+	for( var i=0; i< arr.length; i++ ){
+		if(arr[i].match(/^(?=.*[A-Z])/)){
+			long = arr[i].length;
+		}
+	}
+	return long;
+}
