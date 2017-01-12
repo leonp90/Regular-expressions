6173fca8081ca85caa8cc8699d3d7b2b684bba63
Oscar Leon <oscar.l.p.7@gmail.com>
2017-01-13 00:36:31 +0100

    Regex comments

--- filterString.js
+++ filterString.js
@@ -1,8 +1,10 @@
 function string(S){
+	//Regular expression /[0-9]/g matches any number.
 	var password = S.replace(/[0-9]/g, " ");
 	var substring = password.split(" ");
 	var longest = -1;
 
+	//Regular expression /^(?=.*[A-Z])/ matches any upper case from A to Z.
 	substring.forEach(function(element){
 		let upperCase = /^(?=.*[A-Z])/;
 
@@ -11,6 +13,7 @@ function string(S){
 		};
 
 	});
+
 	return longest;
-};
+}
 	
